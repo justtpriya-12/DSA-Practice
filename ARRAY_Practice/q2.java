217e9c3cbad4f67cf24110d28d0790b34f3be99d
justtpriya-12 <priyachauhan51574@gmail.com>
2025-09-01 01:31:52 +0530

    smallest number in an array

---
+++ ARRAY_Practice/q2.java
@@ -0,0 +1,23 @@
+package ARRAY_Practice;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int min = arr[0];
+        for(int i = 0; i < n;i++){
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println("Smallest number in array :"+min);
+    }
+    
+}
