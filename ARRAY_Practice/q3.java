b80bf1a1efb10fb632a7b71785370ffa78c7133b
justtpriya-12 <priyachauhan51574@gmail.com>
2025-09-01 01:38:10 +0530

    Largest number in an array

---
+++ ARRAY_Practice/q3.java
@@ -0,0 +1,24 @@
+package ARRAY_Practice;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest element in an array :"+max);
+    }
+    
+}
