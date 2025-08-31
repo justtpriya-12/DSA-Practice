1cb3b3fa3cdc54cb3b1c007d40b8ca5f54214e41
justtpriya-12 <priyachauhan51574@gmail.com>
2025-09-01 01:24:30 +0530

    Sum all element of array

---
+++ ARRAY_Practice/q1.java
@@ -0,0 +1,19 @@
+package ARRAY_Practice;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            sum = sum+arr[i];
+        }
+        System.out.println("Sum :"+sum);
+    }
+    
+}
