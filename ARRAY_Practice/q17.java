4d7cb69acdb480369b33e818dfaae84bd6dee472
justtpriya-12 <priyachauhan51574@gmail.com>
2025-09-02 23:19:07 +0530

    most repeated element

--- ARRAY_Practice/q17.java
+++ ARRAY_Practice/q17.java
@@ -1,5 +1,32 @@
 package ARRAY_Practice;
 
+import java.util.Scanner;
+
 public class q17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int element = 0;
+        int max = 0;
+
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count > max){
+                max = count;
+                element = arr[i];
+            }
+        }
+        System.out.println("element "+element+", max "+max);
+    }
     
 }
