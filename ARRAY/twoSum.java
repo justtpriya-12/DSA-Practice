43dc077930b8825210f546ef4ad23bc1875c1f0e
justtpriya-12 <priyachauhan51574@gmail.com>
2025-10-31 00:22:48 +0530

    practice

---
+++ ARRAY/twoSum.java
@@ -0,0 +1,23 @@
+package ARRAY;
+
+import java.util.Scanner;
+
+public class twoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] + arr[j] == target){
+                    System.out.println(i+", "+j);
+                }
+            }
+        }
+    }
+}
---
+++ MathsBasic/LCM.java
@@ -0,0 +1,5 @@
+package MathsBasic;
+
+public class LCM {
+    
+}
